e8db93ba5b22ee525929290ece5ef58ed3e9b01c
sandeep.rana <sandeep.rana@ranasoftcraft.co.in>
2024-08-14 17:41:01 +0530

    bootstarp

---
+++ src/main/java/com/ranasoftcraft/org/spring/boot/google/api/spring_boot_google_calendar_api_starter/SpringBootGoogleCalendarApiStarterApplication.java
@@ -0,0 +1,13 @@
+package com.ranasoftcraft.org.spring.boot.google.api.spring_boot_google_calendar_api_starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGoogleCalendarApiStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGoogleCalendarApiStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ranasoftcraft/org/spring/boot/google/api/spring_boot_google_calendar_api_starter/SpringBootGoogleCalendarApiStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ranasoftcraft.org.spring.boot.google.api.spring_boot_google_calendar_api_starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGoogleCalendarApiStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
